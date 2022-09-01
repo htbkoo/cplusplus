1c4f9ee91b50874bf091863eb2b84a6f130a477c
HeySP6 <h1017760@connect.hku.hk>
2022-09-01 13:27:24 +0200

    solved `leetcode\leetcoding_challenge\2022\sep2022\week1\count_good_nodes_in_binary_tree.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2022/sep2022/week1/count_good_nodes_in_binary_tree.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int goodNodes(TreeNode* root) {
+        return count(root, numeric_limits<int>::min());
+    }
+
+    int count(TreeNode* root, int prevMax) {
+        if (!root) {
+            return 0;
+        }
+
+        int newMax = max(prevMax, root->val);
+
+        int c = root->val >= prevMax ? 1 : 0;
+        int left = count(root->left, newMax);
+        int right = count(root->right, newMax);
+
+        return c + left + right;
+    }
+};
