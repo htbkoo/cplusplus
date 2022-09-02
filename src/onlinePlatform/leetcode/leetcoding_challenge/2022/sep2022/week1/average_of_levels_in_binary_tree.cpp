fd476b652780100568c57a969c67c8ff99fca66d
HeySP6 <h1017760@connect.hku.hk>
2022-09-02 21:09:49 +0200

    solved `leetcode\leetcoding_challenge\2022\sep2022\week1\average_of_levels_in_binary_tree.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2022/sep2022/week1/average_of_levels_in_binary_tree.cpp
@@ -0,0 +1,67 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        vector<double> answer;
+        
+        if (root) {
+            queue<TreeNode*> q;
+            q.push(root);
+            
+            while (q.size() > 0) {
+                long layerSum = 0;
+                int layerCount = q.size();
+
+                for (auto i = 0; i < layerCount; i++) {
+                    auto node = q.front();
+                    q.pop();
+
+                    layerSum += node->val;
+
+                    if (node->left) {
+                        q.push(node->left);
+                    }
+                    if (node->right) {
+                        q.push(node->right);
+                    }
+                }                
+
+                answer.push_back((double) layerSum / (double) layerCount);
+            }
+        }
+        
+        return answer;
+    }
+};
