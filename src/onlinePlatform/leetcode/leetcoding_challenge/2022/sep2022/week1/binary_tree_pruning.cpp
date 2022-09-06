8986482cefea1cf100eab140dacd12e663e733a8
HeySP6 <h1017760@connect.hku.hk>
2022-09-06 06:03:17 +0200

    solved `leetcode/leetcoding_challenge/2022/sep2022/week1/binary_tree_pruning.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2022/sep2022/week1/binary_tree_pruning.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* pruneTree(TreeNode* root) {
+        if (!root) {
+            return root;
+        }
+
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
+
+        if (root->val != 1 && !root->left && !root->right) {
+            return nullptr;
+        }
+
+        return root;
+    }
+};
