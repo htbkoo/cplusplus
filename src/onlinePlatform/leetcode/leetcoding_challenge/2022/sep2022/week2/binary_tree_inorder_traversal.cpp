c7c2832f4cf2a64e9a30247ee535e7b26e32f5a9
HeySP6 <h1017760@connect.hku.hk>
2022-09-09 16:32:15 +0200

    solved `leetcode/leetcoding_challenge/2022/sep2022/week2/binary_tree_inorder_traversal.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2022/sep2022/week2/binary_tree_inorder_traversal.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> v;
+        
+        if (root) {        
+            vector<int> left = inorderTraversal(root->left);
+            v.insert(v.end(), left.begin(), left.end());
+
+            v.push_back(root->val);
+
+            vector<int> right = inorderTraversal(root->right);
+            v.insert(v.end(), right.begin(), right.end());
+        }
+        
+        return v;
+    }
+};
