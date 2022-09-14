416b83da65d4e9d14017ab7b51bd182a958c4f79
HeySP6 <h1017760@connect.hku.hk>
2022-09-14 18:21:10 +0200

    solved `onlinePlatform/codewars/8kyu/l1_set_alarm.cpp`

---
+++ src/onlinePlatform/codewars/8kyu/l1_set_alarm.cpp
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/568dcc3c7f12767a62000038/train/cpp
+
+#include <string>
+
+bool set_alarm(const bool& employed,const bool& vacation){
+  return employed && !vacation;
+}
