5ada949ab8085e01e0d529c6a0ff10ecc109607d
HeySP6 <h1017760+github@connect.hku.hk>
2022-10-05 12:20:29 +0200

    solved `leetcode/leetcoding_challenge/2022/oct2022/week1/path_sum.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2022/oct2022/week1/path_sum.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == nullptr) {
+            return false;
+        }
+        
+        bool isLeaf = root->left == nullptr && root->right == nullptr;
+        
+        if (isLeaf) {
+            return targetSum == root->val;
+        }
+        
+        if (root->left != nullptr && hasPathSum(root->left, targetSum - root->val)) {
+            return true;
+        }
+        
+        if (root->right != nullptr && hasPathSum(root->right, targetSum - root->val)) {
+            return true;
+        }
+        
+        return false;
+    }
+};
