148df74fc6321c0616449963d8c70ea65b2f5d79
HeySP6 <h1017760+github@connect.hku.hk>
2022-10-10 15:25:56 +0200

    solved `leetcode/leetcoding_challenge/2022/oct2022/week2/break_a_palindrome.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2022/oct2022/week2/break_a_palindrome.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        string IMPOSSIBLE = "";
+        if (palindrome.size() == 1) {
+            return IMPOSSIBLE;
+        }
+
+        int left = 0;
+        int right = palindrome.size() - 1;
+        
+        while (left < right) {
+            if (palindrome[left] != 'a' && palindrome[right] != 'a') {
+                return palindrome.substr(0, left) + "a" + palindrome.substr(left + 1, palindrome.size() - 1 - left); 
+            }
+            left++;
+            right--;
+        }
+        
+        return palindrome.substr(0, palindrome.size() - 1) + "b";
+    }
+};
