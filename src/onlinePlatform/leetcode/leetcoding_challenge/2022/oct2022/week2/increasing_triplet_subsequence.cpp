9e7314e6a495a400b007d8555373e4720bd38ff8
HeySP6 <h1017760+github@connect.hku.hk>
2022-10-11 14:46:30 +0200

    solved `leetcode/leetcoding_challenge/2022/oct2022/week2/increasing_triplet_subsequence.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2022/oct2022/week2/increasing_triplet_subsequence.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        vector<bool> hasSmallerLeft = vector<bool>(nums.size());
+        
+        int currMin = numeric_limits<int>::max();        
+        for (int i = 0; i < nums.size(); i++) {
+            if (currMin < nums[i]) {
+                hasSmallerLeft[i] = true;
+            }
+            currMin = min(currMin, nums[i]);
+        }
+        
+        int currMax = numeric_limits<int>::min();                
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            if (currMax > nums[i] && hasSmallerLeft[i]) {
+                return true;
+            }
+            currMax = max(currMax, nums[i]);
+        }
+        
+        return false;
+    }
+};
