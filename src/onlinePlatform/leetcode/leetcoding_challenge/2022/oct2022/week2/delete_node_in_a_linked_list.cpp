8b9fad7568b2e7a422715e8d9ae0390a6a2387f9
HeySP6 <h1017760+github@connect.hku.hk>
2022-10-13 14:26:50 +0200

    solved `leetcode/leetcoding_challenge/2022/oct2022/week2/delete_node_in_a_linked_list.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2022/oct2022/week2/delete_node_in_a_linked_list.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        if (node == nullptr || node->next == nullptr) {
+            throw invalid_argument("expected node to have size longer than 1");
+        }
+        
+        while (node && node->next && node->next->next) {
+            node->val = node->next->val;
+            node = node->next;
+        }
+
+        node->val = node->next->val;
+        node->next = nullptr;
+    }
+};
