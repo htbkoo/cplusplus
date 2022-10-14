485b4d49bebbfcbea58468b0fc1517d5a8a1d1b3
HeySP6 <h1017760+github@connect.hku.hk>
2022-10-14 03:43:47 +0200

    solved `leetcode/leetcoding_challenge/2022/oct2022/week2/delete_the_middle_node_of_a_linked_list.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2022/oct2022/week2/delete_the_middle_node_of_a_linked_list.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+        if (head == nullptr) {
+            return head;
+        }
+        
+        ListNode* dummy = new ListNode(0, head);
+        
+        ListNode* fast = dummy;
+        ListNode* slow = dummy;
+        
+        while (fast != nullptr && fast->next != nullptr && fast->next->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        
+        slow->next = slow->next->next;
+        
+        return dummy->next;
+    }
+};
