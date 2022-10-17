0dd7d0a11b38cf097c922cc2db6b8a77d9a753a2
HeySP6 <h1017760+github@connect.hku.hk>
2022-10-17 12:33:53 +0200

    solved `leetcode/leetcoding_challenge/2022/oct2022/week3/check_if_the_sentence_is_pangram.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2022/oct2022/week3/check_if_the_sentence_is_pangram.cpp
@@ -0,0 +1,25 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+class Solution {
+public:
+    bool checkIfPangram(string sentence) {
+        unordered_set<char> chars;
+        
+        for (char ch: sentence) {
+            chars.insert(ch);
+        }
+    
+        return chars.size() == 26;
+    }
+};
