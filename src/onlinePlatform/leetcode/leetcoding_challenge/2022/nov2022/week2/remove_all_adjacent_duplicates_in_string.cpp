a35eecdae830bcf81f373b2cf2e488adcd0ab234
HeySP6 <h1017760+github@connect.hku.hk>
2022-11-10 08:01:06 +0100

    solved `leetcode/leetcoding_challenge/2022/nov2022/week2/remove_all_adjacent_duplicates_in_string.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2022/nov2022/week2/remove_all_adjacent_duplicates_in_string.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        vector<char> stack;
+        
+        for (char ch: s) {
+            if (stack.size() > 0 && stack.back() == ch) {
+                stack.pop_back();
+            } else {
+                stack.push_back(ch);
+            }
+        }
+        
+        return string(stack.begin(), stack.end());
+    }
+};
