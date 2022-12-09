2248d1207a648bf24a320734b9b656f26c94b916
htbkoo <h1017760+github@connect.hku.hk>
2022-12-09 08:45:43 +0100

    solved `leetcode/leetcoding_challenge/2022/dec2022/week1/range_sum_of_bst.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2022/dec2022/week1/range_sum_of_bst.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if (root == nullptr) {
+            return 0;
+        }
+        
+        int total = 0;
+        
+        int val = root->val;
+        
+        if (low <= val && val <= high) {
+            total += val;
+        }
+        
+        if (val >= low) {
+            total += rangeSumBST(root->left, low, high);
+        }
+        if (val <= high) {
+            total += rangeSumBST(root->right, low, high);
+        }
+        
+        return total;
+    }
+};
