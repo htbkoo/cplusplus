1146623fcf52cdaee767eea9ba8973eb9229beac
htbkoo <h1017760+github@connect.hku.hk>
2022-12-11 10:29:30 +0100

    solved `leetcode/leetcoding_challenge/2022/dec2022/week2/binary_tree_maximum_path_sum.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2022/dec2022/week2/binary_tree_maximum_path_sum.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode* root) {
+        answer = numeric_limits<int>::min();
+        
+        findBest(root);
+        
+        return answer;
+    }
+    
+private:
+    int answer;
+    int findBest(TreeNode* node) {
+        if (node) {
+            int left = findBest(node->left);
+            int right = findBest(node->right);
+            
+            answer = max(answer, node->val + left + right);
+            
+            return max(0, node->val + max(left, right));
+        } else {
+            return 0;
+        }
+    }
+};
