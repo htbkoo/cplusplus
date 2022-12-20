68150db3f5fc802419be1b745c3e0aa937844ed9
htbkoo <h1017760+github@connect.hku.hk>
2022-12-20 09:28:21 +0100

    solved `leetcode/leetcoding_challenge/2022/dec2022/week3/keys_and_rooms.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2022/dec2022/week3/keys_and_rooms.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        unordered_set<int> keys = unordered_set<int>({0});
+        unordered_set<int> visited;
+
+        vector<int> stack = {0};
+
+        while (stack.size() > 0) {
+            int room = stack.back();
+            stack.pop_back();
+
+            if (visited.count(room) > 0) {
+                continue;
+            }
+            visited.insert(room);
+
+            for (int pickUp: rooms[room]) {
+                keys.insert(pickUp);
+                stack.push_back(pickUp);
+            }
+        }
+
+        return keys.size() == rooms.size();
+    }
+};
