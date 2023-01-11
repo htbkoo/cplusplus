d6a11cf29ae485e00656e524fffb15a6afa2f1f9
htbkoo <h1017760+github@connect.hku.hk>
2023-01-11 17:54:00 +0100

    added another solution for `leetcode/leetcoding_challenge/2023/jan2023/week2/max_points_on_a_line.cpp`

--- src/onlinePlatform/leetcode/leetcoding_challenge/2023/jan2023/week2/max_points_on_a_line.cpp
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/jan2023/week2/max_points_on_a_line.cpp
@@ -31,6 +31,35 @@ private:
         for (int i = 0; i < points.size(); i++) {
             if (i != baseIdx) {
                 int x = points[i][0], y = points[i][1];
+                double slope = atan2(y - by, x - bx);
+                counter[slope]++;
+                maxCount = max(maxCount, counter[slope]);
+            }
+        }
+        return maxCount;
+    }
+};
+
+class FirstSolution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int answer = 0;
+        for (int i = 0; i < points.size(); i++) {
+            answer = max(answer, 1 + getCount(points, i));
+        }
+        return answer;
+    }
+
+private:
+    int getCount(vector<vector<int>>& points, int baseIdx) {
+        int maxCount = 0;
+        int verticalCount = 0;
+        unordered_map<double, int> counter;
+
+        int bx = points[baseIdx][0], by = points[baseIdx][1];
+        for (int i = 0; i < points.size(); i++) {
+            if (i != baseIdx) {
+                int x = points[i][0], y = points[i][1];
                 if (x == bx) {
                     // vertical
                     verticalCount++;
