e5b40121730e541b6c4417d240b1bb6a1dbb4108
htbkoo <h1017760+github@connect.hku.hk>
2023-01-11 17:53:37 +0100

    solved `leetcode/leetcoding_challenge/2023/jan2023/week2/max_points_on_a_line.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/jan2023/week2/max_points_on_a_line.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int answer = 0;
+        for (int i = 0; i < points.size(); i++) {
+            answer = max(answer, 1 + getCount(points, i));
+        }
+        return answer;
+    }
+
+private:
+    int getCount(vector<vector<int>>& points, int baseIdx) {
+        int maxCount = 0;
+        int verticalCount = 0;
+        unordered_map<double, int> counter;
+
+        int bx = points[baseIdx][0], by = points[baseIdx][1];
+        for (int i = 0; i < points.size(); i++) {
+            if (i != baseIdx) {
+                int x = points[i][0], y = points[i][1];
+                if (x == bx) {
+                    // vertical
+                    verticalCount++;
+                    maxCount = max(maxCount, verticalCount);
+                } else {
+                    // TODO: handle precision problem
+                    double slope = (double) (y - by) / (double) (x - bx);
+                    counter[slope]++;
+                    maxCount = max(maxCount, counter[slope]);
+                }
+            }
+        }
+        return maxCount;
+    }
+};
