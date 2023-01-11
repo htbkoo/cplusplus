a60dd98add2b86f1bb100e9db505480e3d7b383f
htbkoo <h1017760+github@connect.hku.hk>
2023-01-11 17:54:38 +0100

    solved `leetcode/leetcoding_challenge/2023/jan2023/week2/binary_tree_preorder_traversal.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/jan2023/week2/binary_tree_preorder_traversal.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> answer;
+        preorder(root, answer);
+        return answer;
+    }
+
+private:
+    void preorder(TreeNode* root, vector<int>& v) {
+        if (root != nullptr) {
+            v.push_back(root->val);
+            preorder(root->left, v);
+            preorder(root->right, v);
+        }
+    }
+};
