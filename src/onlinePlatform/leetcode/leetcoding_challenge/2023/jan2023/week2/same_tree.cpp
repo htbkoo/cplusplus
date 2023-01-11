aca4b08f407796bc0a88186cce28cda302fc13a0
htbkoo <h1017760+github@connect.hku.hk>
2023-01-11 17:55:26 +0100

    solved `leetcode/leetcoding_challenge/2023/jan2023/week2/same_tree.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/jan2023/week2/same_tree.cpp
@@ -0,0 +1,36 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) {
+            return true;
+        }
+        if (p == nullptr || q == nullptr) {
+            return false;
+        }
+        return p->val == q-> val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
