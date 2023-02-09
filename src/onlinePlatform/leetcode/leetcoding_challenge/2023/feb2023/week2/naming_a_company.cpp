ea34c1cea5b520363782e391c8493f416c799529
htbkoo <h1017760+github@connect.hku.hk>
2023-02-09 23:09:28 +0100

    solved `leetcode/leetcoding_challenge/2023/feb2023/week2/naming_a_company`

--- src/onlinePlatform/leetcode/leetcoding_challenge/2023/feb2023/week2/naming_a_company.cpp
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/feb2023/week2/naming_a_company.cpp
@@ -11,6 +11,99 @@
 #include <iostream> // includes cin to read from stdin and cout to write to stdout
 using namespace std; // since cin and cout are both in namespace std, this saves some text
 
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        unordered_map<char, unordered_set<string>> firstCharToTail;
+        unordered_set<char> allCharsSet;
+        for (auto &idea: ideas) {
+            char firstChar = idea[0];
+            string tail = idea.substr(1, idea.size() - 1);
+            firstCharToTail[firstChar].insert(tail);
+            allCharsSet.insert(firstChar);
+        }
+        
+        long long answer = 0;
+        vector<char> allChars(allCharsSet.begin(), allCharsSet.end());
+        for (int i = 0; i < allChars.size(); i++) {
+            for (int j = i + 1; j < allChars.size(); j++) {
+                char ch1 = allChars[i], ch2 = allChars[j];
+                unordered_set<string> set1 = firstCharToTail[ch1], set2 = firstCharToTail[ch2];
+                
+                long long count1 = set1.size(), count2 = set2.size();
+                for (auto tail1: set1) {
+                    if (set2.count(tail1) > 0) {
+                        count1--;
+                        count2--;
+                    }
+                }
+                
+                answer += 2 * count1 * count2;
+            }
+        }
+        
+        return answer;
+    }
+};
+
+class WASolution3 {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        unordered_map<string, unordered_set<char>> tailToFirstChars;
+        unordered_map<char, long long> firstCharCounter;
+        
+        for (auto &idea: ideas) {
+            char firstChar = idea[0];
+            string tail = idea.substr(1, idea.size() - 1);
+            tailToFirstChars[tail].insert(firstChar);
+            firstCharCounter[firstChar]++;
+        }
+        
+        long long answer = 0;
+        for (auto &idea: ideas) {
+            char firstChar = idea[0];
+            string tail = idea.substr(1, idea.size() - 1);
+            for (auto &[otherFirstChar, count]: firstCharCounter) {
+                bool isCompatible = firstChar != otherFirstChar && tailToFirstChars[tail].count(otherFirstChar) == 0;
+                if (isCompatible) {
+                    answer += count;
+                }
+            }
+        }
+        
+        return answer;
+    }
+};
+
+class WASolution2 {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        unordered_map<string, unordered_set<char>> tailToFirstChars;
+        unordered_map<char, long long> firstCharCounter;
+        
+        for (auto &idea: ideas) {
+            char firstChar = idea[0];
+            string tail = idea.substr(1, idea.size() - 1);
+            tailToFirstChars[tail].insert(firstChar);
+            firstCharCounter[firstChar]++;
+        }
+        
+        long long answer = 0;
+        for (auto &idea: ideas) {
+            char firstChar = idea[0];
+            string tail = idea.substr(1, idea.size() - 1);
+            for (auto &[otherFirstChar, count]: firstCharCounter) {
+                bool isCompatible = firstChar != otherFirstChar && tailToFirstChars[tail].count(otherFirstChar) == 0;
+                if (isCompatible) {
+                    answer += count;
+                }
+            }
+        }
+        
+        return answer;
+    }
+};
+
 template <typename Map>
 void printMap(Map &m) {
     cout << "{" << endl;
@@ -40,7 +133,7 @@ void printMapWithIterable(Map &m) {
     cout << "}" << endl;
 }
 
-class Solution {
+class WASolution {
 public:
     long long distinctNames(vector<string>& ideas) {
         unordered_map<string, unordered_set<char>> tailToFirstChars;
