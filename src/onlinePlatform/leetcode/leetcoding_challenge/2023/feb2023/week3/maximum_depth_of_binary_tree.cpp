025e7e1cbe4990d8592ba2048cdd17693cf2b896
htbkoo <h1017760+github@connect.hku.hk>
2023-02-16 01:17:55 +0100

    solved `leetcode/leetcoding_challenge/2023/feb2023/week3/maximum_depth_of_binary_tree`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/feb2023/week3/maximum_depth_of_binary_tree.cpp
@@ -0,0 +1,46 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;            
+        }
+        
+        return 1 + max(
+            maxDepth(root->left),
+            maxDepth(root->right)
+        );
+    }
+};
