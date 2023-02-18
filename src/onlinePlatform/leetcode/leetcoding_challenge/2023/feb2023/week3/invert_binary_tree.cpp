9570f88070db96b61426741079f503eb3f94299e
htbkoo <h1017760+github@connect.hku.hk>
2023-02-18 08:12:28 +0100

    solved `leetcode/leetcoding_challenge/2023/feb2023/week3/invert_binary_tree`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/feb2023/week3/invert_binary_tree.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root != nullptr) {
+            TreeNode* temp = root->left;
+            root->left = root->right;
+            root->right = temp;
+            
+            invertTree(root->left);
+            invertTree(root->right);
+        }
+        
+        return root;
+    }
+};
