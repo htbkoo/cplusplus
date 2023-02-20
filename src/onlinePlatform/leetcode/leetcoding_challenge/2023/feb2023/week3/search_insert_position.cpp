d03bd35fbad60cf95d6f62708c63d1084d0b09f3
htbkoo <h1017760+github@connect.hku.hk>
2023-02-20 09:15:44 +0100

    added another solution for `leetcode/leetcoding_challenge/2023/feb2023/week3/search_insert_position`

--- src/onlinePlatform/leetcode/leetcoding_challenge/2023/feb2023/week3/search_insert_position.cpp
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/feb2023/week3/search_insert_position.cpp
@@ -14,8 +14,45 @@ using namespace std; // since cin and cout are both in namespace std, this saves
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
+        int lo = 0, hi = nums.size() - 1;
+        
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            if (target < nums[mid]) {
+                hi = mid;
+            } else if (target > nums[mid]) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+            
+        }
+        
+        if (target > nums[lo]) {
+            return lo + 1;
+        } else {
+            return lo;
+        }
+    }
+};
+
+class FirstSolution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
         auto it = lower_bound(nums.begin(), nums.end(), target);
         
         return distance(nums.begin(), it);
     }
 };
+
+int main() {
+    Solution soln;
+
+    vector<int> nums = {1,3,5,6};
+    int target = 5;
+
+    cout << soln.searchInsert(nums, target) << endl;
+
+    return 0;
+}
