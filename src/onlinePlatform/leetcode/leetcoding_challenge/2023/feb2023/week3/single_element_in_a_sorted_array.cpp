0c6f909369444867856b6f1e45edbcbbe42a26fe
htbkoo <h1017760+github@connect.hku.hk>
2023-02-21 07:48:33 +0100

    solved `leetcode/leetcoding_challenge/2023/feb2023/week3/single_element_in_a_sorted_array`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/feb2023/week3/single_element_in_a_sorted_array.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int answer = 0;
+        for (int num: nums) {
+            answer ^= num;
+        }
+        return answer;
+    }
+};
