ff651e795186bf5a70fd3d4ac2a5d22a782bb915
htbkoo <h1017760+github@connect.hku.hk>
2023-02-21 08:10:27 +0100

    added another solution for `leetcode/leetcoding_challenge/2023/feb2023/week3/single_element_in_a_sorted_array`

--- src/onlinePlatform/leetcode/leetcoding_challenge/2023/feb2023/week3/single_element_in_a_sorted_array.cpp
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/feb2023/week3/single_element_in_a_sorted_array.cpp
@@ -14,6 +14,35 @@ using namespace std; // since cin and cout are both in namespace std, this saves
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+        int lo = 0, hi = nums.size() - 1;
+        
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            
+            bool isLeftFulfill = mid == 0 || nums[mid] != nums[mid - 1];
+            bool isRightFulfill = mid == nums.size() - 1 || nums[mid] != nums[mid + 1];
+            
+            if (isLeftFulfill && isRightFulfill) {
+                return nums[mid];
+            } else if (!isLeftFulfill && !isRightFulfill) {
+                throw invalid_argument("there are no matches, final posisition: " + to_string(mid));
+            } else {
+                bool shouldRightEqual = mid % 2 == 0;                
+                if (shouldRightEqual == isLeftFulfill) {
+                    lo = mid + 1;                    
+                } else {
+                    hi = mid - 1;
+                } 
+            } 
+        }
+        
+        throw invalid_argument("there are no matches, final posisition: " + to_string(lo));
+    }
+};
+
+class FirstSolution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
         int answer = 0;
         for (int num: nums) {
             answer ^= num;
