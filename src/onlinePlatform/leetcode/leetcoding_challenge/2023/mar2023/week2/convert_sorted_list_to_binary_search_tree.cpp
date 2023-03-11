661b55287803e7c586656b43e89f3ff25e9eeb2b
htbkoo <h1017760+github@connect.hku.hk>
2023-03-11 07:39:08 +0100

    solved `leetcode/leetcoding_challenge/2023/mar2023/week2/convert_sorted_list_to_binary_search_tree`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/mar2023/week2/convert_sorted_list_to_binary_search_tree.cpp
@@ -0,0 +1,84 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+#include <random>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        vector<int> v = asVector(head);
+        
+        return asBST(v, 0, v.size() - 1);
+    }
+    
+private:
+    vector<int> asVector(ListNode* head) {        
+        vector<int> v;
+        while (head != nullptr) {
+            v.push_back(head->val);
+            head = head->next;
+        }        
+        return v;
+    }
+    
+    TreeNode* asBST(vector<int> v, int lo, int hi) {
+        if (lo > hi) {
+            return nullptr;
+        }
+        
+        int mid = lo + (hi - lo) / 2;
+        
+        TreeNode* left = asBST(v, lo, mid - 1);
+        TreeNode* right = asBST(v, mid + 1, hi);
+        TreeNode* BST = new TreeNode(v[mid], left, right);
+        return BST;
+    }
+};
