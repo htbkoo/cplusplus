0f234182234d82ea49eaa1d95cf193942e46d9a4
htbkoo <h1017760+github@connect.hku.hk>
2023-03-12 08:02:29 +0100

    solved `leetcode/leetcoding_challenge/2023/mar2023/week2/merge_k_sorted_lists`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/mar2023/week2/merge_k_sorted_lists.cpp
@@ -0,0 +1,62 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        ListNode* dummy = new ListNode();
+        
+        auto cmp = [](pair<int, ListNode*>& a, pair<int, ListNode*>& b) {return a.first > b.first;};
+        priority_queue<pair<int, ListNode*>, vector<pair<int, ListNode*>>, decltype(cmp)> pq(cmp);
+        for (auto head: lists) {
+            if (head != nullptr) {
+                pq.push(pair{head->val, head});
+            }
+        }
+        
+        ListNode* curr = dummy;
+        while (pq.size() > 0) {
+            auto [_, node] = pq.top();
+            pq.pop();
+            
+            curr->next = node;
+            curr = curr->next;
+            
+            node = node->next;
+            if (node != nullptr) {
+                // pq.push(pair{node->val, node});
+                pq.push(pair<int, ListNode*>{node->val, node});
+            }
+        }
+        
+        return dummy->next;
+    }
+};
