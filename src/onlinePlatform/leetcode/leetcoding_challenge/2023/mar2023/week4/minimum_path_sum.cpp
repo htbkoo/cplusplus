3872d7c3a2a57c2be030d58862a3e2a1eeeb07c8
htbkoo <h1017760+github@connect.hku.hk>
2023-03-27 08:09:54 +0200

    added another solution for `leetcode/leetcoding_challenge/2023/mar2023/week4/minimum_path_sum`

--- src/onlinePlatform/leetcode/leetcoding_challenge/2023/mar2023/week4/minimum_path_sum.cpp
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/mar2023/week4/minimum_path_sum.cpp
@@ -25,6 +25,30 @@ public:
         }
         
         for (int y = 1; y < height; y++) {
+            curr[0] += grid[y][0];
+            for (int x = 1; x < width; x++) {
+                curr[x] = grid[y][x] + min(curr[x - 1], curr[x]);
+            }
+        }
+        
+        return curr[width - 1];
+    }
+};
+
+class FirstSolution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        // TODO: what if grid is empty / nullptr?
+        vector<int> curr = vector<int>(grid[0].begin(), grid[0].end());
+        
+        int height = grid.size();
+        int width = grid[0].size();
+        
+        for (int x = 1; x < width; x++) {
+            curr[x] += curr[x - 1];
+        }
+        
+        for (int y = 1; y < height; y++) {
             vector<int> prev = vector<int>(curr.begin(), curr.end());
             curr[0] = prev[0] + grid[y][0];
             for (int x = 1; x < width; x++) {
