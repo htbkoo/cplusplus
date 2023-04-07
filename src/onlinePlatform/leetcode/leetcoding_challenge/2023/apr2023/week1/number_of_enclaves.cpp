4eaec80a65b204dc34f822b80064a9c52b6eaed6
htbkoo <h1017760+github@connect.hku.hk>
2023-04-07 08:07:13 +0200

    added another solution for `leetcode/leetcoding_challenge/2023/apr2023/week1/number_of_enclaves`

--- src/onlinePlatform/leetcode/leetcoding_challenge/2023/apr2023/week1/number_of_enclaves.cpp
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/apr2023/week1/number_of_enclaves.cpp
@@ -32,6 +32,79 @@ public:
         int numCells = 0;
         for (int y = 1; y < height - 1; y++) {
             for (int x = 1; x < width - 1; x++) {
+                numCells += grid[y][x];
+            }            
+        }
+       
+        return numCells;
+    }
+    
+private:
+    int SEA = 0;
+    vector<pair<int, int>> DIRECTIONS = {
+        pair{0, -1},
+        pair{0, 1},
+        pair{-1, 0},
+        pair{1, 0},
+    };
+    
+    template<typename V>
+    vector<V> deepClone(vector<V> old) {
+        vector<V> vv;
+        
+        for (auto v: old) {
+            vv.push_back(V(v));
+        }
+        
+        return vv;
+    }
+    
+    int floodFill(vector<vector<int>>& grid, int y, int x, int val) {
+        int height = grid.size();
+        int width = grid[0].size();
+        
+        bool isWithinBoundary = 0 <= y && y < height && 0 <= x && x < width;
+        if (!isWithinBoundary) {
+            return 0;
+        }
+        
+        if (grid[y][x] == val) {
+            return 0;
+        }
+        grid[y][x] = val;
+        
+        int count = 1;
+        for (auto& [dy, dx]: DIRECTIONS) {
+            int ny = y + dy;
+            int nx = x + dx;
+            count += floodFill(grid, ny, nx, val);
+        }
+        
+        return count;
+    }
+};
+
+class FirstSolution {
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        grid = deepClone(grid);
+        
+        int height = grid.size();
+        int width = grid[0].size();
+        
+        for (int y = 0; y < height; y++) {
+            floodFill(grid, y, 0, SEA);
+            floodFill(grid, y, width - 1, SEA);
+        }
+        
+        for (int x = 0; x < width; x++) {
+            floodFill(grid, 0, x, SEA);
+            floodFill(grid, height - 1, x, SEA);            
+        }
+        
+        int numCells = 0;
+        for (int y = 1; y < height - 1; y++) {
+            for (int x = 1; x < width - 1; x++) {
                 numCells += floodFill(grid, y, x, SEA);
             }            
         }
