9538292adf7df645f143b34c4e1e2337485372eb
htbkoo <h1017760+github@connect.hku.hk>
2023-04-16 15:37:26 +0200

    solved `leetcode/leetcoding_challenge/2023/apr2023/week3/number_of_ways_to_form_a_target_string_given_a_dictionary`

--- src/onlinePlatform/leetcode/leetcoding_challenge/2023/apr2023/week3/number_of_ways_to_form_a_target_string_given_a_dictionary.cpp
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/apr2023/week3/number_of_ways_to_form_a_target_string_given_a_dictionary.cpp
@@ -14,44 +14,51 @@ using namespace std; // since cin and cout are both in namespace std, this saves
 class Solution {
 public:
     int numWays(vector<string>& words, string target) {
+        if (words.size() == 0) {
+            // TODO: confirm this
+            return 0;
+         } 
+
+        wordSize = words[0].size();
+
         memo = unordered_map<int, unordered_map<int, long long>>();
         
-        charsToPositionToCountMap = unordered_map<char, map<int, int>>();
+        positionToCharsToCountMap = map<int, unordered_map<char, int>>();
         for (auto word: words) {
-            for (int i = 0; i < word.size(); ++i) {
-                char ch = word[i];
-                charsToPositionToCountMap[ch][i]++;
+            for (int k = 0; k < wordSize ; ++k) {
+                char ch = word[k];
+                positionToCharsToCountMap[k][ch]++;
             }
         }
         
-        return count(target, 0, -1) % MOD;
+        return count(target, 0, 0) % MOD;
     }
     
 private:
     long long MOD = pow(10, 9) + 7;
 
     unordered_map<int, unordered_map<int, long long>> memo;
-    unordered_map<char, map<int, int>> charsToPositionToCountMap;
+    map<int, unordered_map<char, int>> positionToCharsToCountMap;
+    int wordSize;
     
     long long count(string& target, int i, int k) {
         if (i == target.size()) {
             return 1;
         }
+        if (k >= wordSize) {
+            return 0;
+        }
         
         if (memo.count(i) > 0 && memo[i].count(k) > 0) {
             return memo[i][k];
         }
-        memo[i][k] = 0;
-        
+
         char ch = target[i];
-        
-        for (auto it = charsToPositionToCountMap[ch].upper_bound(k); it != charsToPositionToCountMap[ch].end(); ++it) {
-            int pos = it->first;
-            int occurrences = it->second;
-            
-            memo[i][k] = (memo[i][k] + (long long) occurrences * count(target, i + 1, pos)) % MOD;
-        }
-        
+        memo[i][k] = (
+            count(target, i, k + 1) 
+            + (positionToCharsToCountMap[k][ch] * count(target, i + 1, k + 1)) % MOD
+        ) % MOD;
+
         return memo[i][k];
     }
 };
