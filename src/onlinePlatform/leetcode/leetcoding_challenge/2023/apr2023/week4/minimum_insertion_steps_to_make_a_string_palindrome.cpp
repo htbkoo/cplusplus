45198e678afae58c279d236acf67ed482fe0aaaa
htbkoo <h1017760+github@connect.hku.hk>
2023-04-23 10:40:50 +0200

    solved `leetcode/leetcoding_challenge/2023/apr2023/week4/minimum_insertion_steps_to_make_a_string_palindrome.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/apr2023/week4/minimum_insertion_steps_to_make_a_string_palindrome.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+const int MAX_S_SIZE = 500;
+const int UNINITIALIZED = -1;
+
+class Solution {
+public:
+    int minInsertions(string s) {
+        memset(memo, UNINITIALIZED, sizeof(memo));
+        return findMin(s, 0, s.size() - 1);
+    }
+    
+private:
+    int memo[MAX_S_SIZE + 1][MAX_S_SIZE + 1];
+    
+    int findMin(string& s, int lo, int hi) {
+        if (memo[lo][hi] != UNINITIALIZED) {
+            return memo[lo][hi];
+        }
+        if (lo >= hi) {
+            return 0;
+        }
+        if (s[lo] == s[hi]) {
+            memo[lo][hi] = findMin(s, lo + 1, hi - 1);
+        } else {
+            memo[lo][hi] = 1 + min(
+                findMin(s, lo + 1, hi),
+                findMin(s, lo, hi - 1)
+            );        
+        }
+        
+        return memo[lo][hi];
+    }
+};
