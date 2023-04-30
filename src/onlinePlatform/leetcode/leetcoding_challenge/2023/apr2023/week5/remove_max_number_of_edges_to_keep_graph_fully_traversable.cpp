87b7e1c554eb8257ac37730c27b1851d6d6a47ce
htbkoo <h1017760+github@connect.hku.hk>
2023-04-30 09:53:51 +0200

    WIP `leetcode/leetcoding_challenge/2023/apr2023/week5/remove_max_number_of_edges_to_keep_graph_fully_traversable.cpp`

--- src/onlinePlatform/leetcode/leetcoding_challenge/2023/apr2023/week5/remove_max_number_of_edges_to_keep_graph_fully_traversable.cpp
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/apr2023/week5/remove_max_number_of_edges_to_keep_graph_fully_traversable.cpp
@@ -15,11 +15,107 @@ using namespace std; // since cin and cout are both in namespace std, this saves
 class Solution {
 public:
     int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
-        vector<vector<pair<int, int>>> edgesByType = {{}, {}, {}, {}};
+        const int EDGE_TYPE_BOTH = 3;
+        const int NOT_FULLY_TRAVERSABLE = -1;
         
+        vector<int> parents = {-1};
+        vector<int> sizes = {-1};
+        for (int i = 1; i <= n; ++i) {
+            parents.push_back(i);
+            sizes.push_back(1);
+        }
+                        
+        vector<vector<pair<int, int>>> edgesByType = {{}, {}, {}, {}};
         for (auto edge: edges) {
             int type = edge[0], u = edge[1], v = edge[2];
             edgesByType[type].push_back(pair{u, v});
+        }
+        
+        int answer = 0;
+
+        answer += countNumRemovableEdgesAndUpdateDisjointSet(parents, sizes, edgesByType[EDGE_TYPE_BOTH]);
+        
+        for (int edgeType = 1; edgeType <= 2; edgeType++) {
+            vector<int> tempParents(parents.begin(), parents.end());
+            vector<int> tempSizes(sizes.begin(), sizes.end());
+            answer += countNumRemovableEdgesAndUpdateDisjointSet(parents, sizes, edgesByType[edgeType]);
+            if (!isFullyTraversable(parents)) {
+                return NOT_FULLY_TRAVERSABLE;
+            }
+        }
+        
+        return answer;
+    }
+    
+private:
+    int find(vector<int>& parents, int x) {
+        if (parents[x] != x) {
+            parents[x] = find(parents, parents[x]);
+        }
+        return parents[x];
+    }
+    
+    bool unionSet(vector<int>& parents, vector<int>& sizes, int x, int y) {
+        x = find(parents, x);
+        y = find(parents, y);
+        
+        if (x == y) {
+            return false;
+        }
+        
+        if (sizes[y] > sizes[x]) {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        
+        parents[y] = x;
+        sizes[x] += sizes[y];
+        return true;
+    }
+    
+    int countNumRemovableEdgesAndUpdateDisjointSet(vector<int>& parents, vector<int>& sizes, vector<pair<int, int>>& edges) {
+        int count = 0;
+        for (auto& [u, v]: edges) {
+            if (!unionSet(parents, sizes, u, v)) {
+                count++;
+            }
+        }
+        return count;
+    }
+    
+    bool isFullyTraversable(vector<int>& parents) {
+        unordered_set<int> s;
+        
+        for (int i = 1; i < parents.size(); i++) {
+            s.insert(parents[i]);
+        }
+        
+        return s.size() == 1;
+    }
+};
+
+class WIPSolution {
+public:
+    int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
+        vector<int> parents = {-1};
+        for (int i = 1; i <= n; ++i) {
+            parents.push_back(i);
+        }
+        
+        int countDuplicatedEdges = 0;
+                
+        unordered_map<int, unordered_map<int, unordered_set<int>>> neighboursByType;
+        
+        for (auto edge: edges) {
+            int type = edge[0], u = edge[1], v = edge[2];
+            bool isDuplicated = neighboursByType.count(type) > 0 && neighboursByType[type].count(u) > 0 && neighboursByType[type][u].count(v) > 0;
+            if (isDuplicated) {
+                countDuplicatedEdges++;
+            } else {
+                neighboursByType[type][u].insert(v);
+                neighboursByType[type][v].insert(u);
+            }
         }
         
         
