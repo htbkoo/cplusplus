ca4661f7e4c9fb73ba452c72e493650e96245586
htbkoo <h1017760+github@connect.hku.hk>
2023-05-16 07:36:47 +0200

    solved `leetcode/leetcoding_challenge/2023/may2023/week3/swap_nodes_in_pairs.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/may2023/week3/swap_nodes_in_pairs.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* dummy = new ListNode(0, head);
+        
+        ListNode* curr = dummy;
+        while (curr != nullptr && curr->next != nullptr && curr->next->next != nullptr) {
+            ListNode* nextNode = curr->next;
+            ListNode* nextNextNode = nextNode->next;
+            
+            curr->next = nextNextNode;
+            nextNode->next = nextNextNode->next;
+            nextNextNode->next = nextNode;
+            
+            curr = nextNode;
+        }
+        
+        return dummy->next;
+    }
+};
