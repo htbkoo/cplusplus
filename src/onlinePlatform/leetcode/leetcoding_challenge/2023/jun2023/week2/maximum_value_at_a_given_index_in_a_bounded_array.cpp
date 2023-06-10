df36c9fc1b3e871313605424df45d087de3ca6e0
htbkoo <h1017760+github@connect.hku.hk>
2023-06-10 11:00:56 +0200

    solved `leetcode/leetcoding_challenge/2023/jun2023/week2/maximum_value_at_a_given_index_in_a_bounded_array`

--- src/onlinePlatform/leetcode/leetcoding_challenge/2023/jun2023/week2/maximum_value_at_a_given_index_in_a_bounded_array.cpp
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/jun2023/week2/maximum_value_at_a_given_index_in_a_bounded_array.cpp
@@ -14,6 +14,8 @@ using namespace std; // since cin and cout are both in namespace std, this saves
 class Solution {
 public:
     int maxValue(int n, int index, int maxSum) {
+        maxSum -= n; // nums[i] is a positive integer where 0 <= i < n.
+
         int lo = 0;
         int hi = maxSum;
         
@@ -22,7 +24,39 @@ public:
             
             long totalSum = calculateTotalSum(n, index, mid);
             
-            cout << mid << " " << totalSum << endl;
+            if (totalSum > maxSum) {
+                hi = mid - 1;
+            } else {
+                lo = mid;
+            }
+        }
+        
+        return lo + 1;
+    }
+    
+private:
+    long calculateTotalSum(int n, int index, int mid) {
+        long leftLen = min(index + 1, mid);
+        long rightLen = min(n - (index + 1), mid - 1);
+
+        long leftSum = leftLen * (max(1L, mid - leftLen + 1L) + mid) / 2L;
+        long rightSum = rightLen * (max(1L, (mid - 1L) - rightLen + 1L) + (mid - 1L)) / 2L;
+        
+        return leftSum + rightSum;
+    }
+};
+
+class WASolution {
+public:
+    int maxValue(int n, int index, int maxSum) {
+        int lo = 0;
+        int hi = maxSum;
+        
+        while (lo < hi) {
+            int mid = lo + (hi - lo + 1) / 2;
+            
+            long totalSum = calculateTotalSum(n, index, mid);
+            
             if (totalSum > maxSum) {
                 hi = mid - 1;
             } else {
