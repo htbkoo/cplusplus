dbded15258d847ebde534abca40ace112101f8db
htbkoo <h1017760+github@connect.hku.hk>
2023-06-17 23:08:27 +0200

    WIP `leetcode/leetcoding_challenge/2023/jun2023/week3/make_array_strictly_increasing`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/jun2023/week3/make_array_strictly_increasing.cpp
@@ -0,0 +1,101 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+const int UNINITIALIZED = -1;
+const int IMPOSSIBLE = numeric_limits<int>::max() / 2;
+
+class Solution {
+public:
+    int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
+        arr2 = vector(arr2.begin(), arr2.end());
+        sort(arr2.begin(), arr2.end());
+        
+        memo = vector<vector<int>>();
+        for (int i = 0; i <= arr1.size(); ++i) {
+            memo.push_back(vector<int>((arr2.size() + 1) * 2, UNINITIALIZED));
+        }
+        
+        int answer = findMin(arr1, arr2, 0, 0);
+        if (answer == IMPOSSIBLE) {
+            return -1;
+        } else {
+            return answer;
+        }
+    }
+    
+private:
+    vector<vector<int>> memo;
+    
+    int findMin(vector<int>& arr1, vector<int>& arr2, int i, int j) {
+        if (i >= arr1.size()) {
+            return 0;
+        }
+        
+cout << i << " " << j <<endl;
+
+        if (memo[i][j] != UNINITIALIZED) {
+            return memo[i][j];
+        }
+        
+        memo[i][j] = IMPOSSIBLE;
+                
+        if (isValid(arr1, arr2, i, j)) {
+            int nextJ = hasReplaced(j) ? j + 1 : j;
+            memo[i][j] = min(
+                memo[i][j],
+                findMin(arr1, arr2, i + 1, nextJ)
+            );
+        }
+        
+        bool canReplace = j < 2 * arr2.size();
+        if (canReplace) {
+            int answer = IMPOSSIBLE;
+            if (isValid(arr1, arr2, i, j + 1)) {
+                answer = 1 + findMin(arr1, arr2, i + 1, j + 1);
+            } else {
+                answer = findMin(arr1, arr2, i, j + 1);
+            }
+                        
+            memo[i][j] = min(
+                memo[i][j],
+                answer
+            );
+        }
+        
+        return memo[i][j];
+    }
+    
+    bool isValid(vector<int>& arr1, vector<int>& arr2, int i, int j) {
+        if (i == 0) {
+            return true;
+        } else {
+            int prevNum = hasReplaced(j) ? arr2[(j - 1) / 2] : arr1[i - 1];
+            return arr1[i] > prevNum;
+        }
+    }
+    
+    bool hasReplaced(int j) {
+        return j % 2 != 0;
+    }
+};
+
+int main() {
+    Solution soln;
+
+    vector<int> arr1 = {1,5,3,6,7};
+    vector<int> arr2 = {1,3,2,4};
+
+    cout << soln.makeArrayIncreasing(arr1, arr2) << endl;
+
+    return 0;
+}
