0c8a39a02da15bc4960f099ff9935f85cd141121
htbkoo <h1017760+github@connect.hku.hk>
2023-07-16 03:05:22 +0200

    WIP `leetcode/leetcoding_challenge/2023/jul2023/week3/smallest_sufficient_team`

--- src/onlinePlatform/leetcode/leetcoding_challenge/2023/jul2023/week3/smallest_sufficient_team.cpp
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/jul2023/week3/smallest_sufficient_team.cpp
@@ -16,6 +16,106 @@ const int UNINITIALIZED = -1;
 class Solution {
 public:
     vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {        
+        memo = vector<vector<long long>>();
+        for (int i = 0; i < people.size(); ++i) {
+            memo.push_back(vector<long long>(getBitmaskSize(req_skills), UNINITIALIZED));
+        }
+        
+        skillsIndices = unordered_map<string, int>();
+        for (int i = 0; i < req_skills.size(); ++i) {
+            string skill = req_skills[i];
+            skillsIndices[skill] = i;
+        }
+        
+        return toVector(findSmallest(0, 0, req_skills, people));
+    }
+    
+private:
+    unordered_map<string, int> skillsIndices;
+    vector<vector<long long>> memo;
+    
+    vector<int> toVector(long long peopleBitmask) {
+        vector<int> answer;
+        int currBit = 0;
+        while (peopleBitmask > 0) {
+            if (peopleBitmask % 2 == 1) {
+                answer.push_back(currBit);
+            }
+            
+            peopleBitmask /= 2;
+            currBit++;
+        }
+        
+        return answer;
+    }
+    
+    long long findSmallest(int i, int bitmask, vector<string>& req_skills, vector<vector<string>>& people) {
+        if (isSufficientTeam(bitmask, req_skills)) {
+            return 0;
+        }
+        
+        if (i == people.size()) {
+            // throw invalid_argument("unable to form a sufficient team");
+            return pow(2l, people.size()) - 1;
+        }
+        
+        if (memo[i][bitmask] == UNINITIALIZED) {
+            long long skipPerson = findSmallest(i + 1, bitmask, req_skills, people);
+            long long includePerson = withPerson(i, findSmallest(i + 1, withSkills(people[i], bitmask), req_skills, people));
+
+            if (getNumPeople(skipPerson) <= getNumPeople(includePerson)) {
+                memo[i][bitmask] = skipPerson;
+            } else {
+                memo[i][bitmask] =  includePerson;
+            }
+        }
+        
+        return memo[i][bitmask];
+    }
+    
+    int getBitmaskSize(vector<string>& req_skills) {
+        return pow(2, req_skills.size());
+    }
+    
+    int withSkills(vector<string>& skills, int bitmask) {
+        for (auto skill: skills) {
+            bitmask = withSkill(skill, bitmask);
+        }
+        return bitmask;
+    }
+    
+    long long withPerson(int i, long long peopleBitmask) {
+        return (long long) pow(2l, i) | peopleBitmask;
+    }
+    
+    int withSkill(string skill, int bitmask) {
+        int skillIdx = skillsIndices[skill];
+        return bitmask | (int) pow(2, skillIdx);
+    }
+    
+    bool isSufficientTeam(int bitmask, vector<string>& req_skills) {
+        return bitmask == (getBitmaskSize(req_skills) - 1);
+    }
+    
+    int getNumPeople(long long peopleBitmask) {
+        int numPeople = 0;
+        int currBit = 0;
+        while (peopleBitmask > 0) {
+            if (peopleBitmask % 2 == 1) {
+                numPeople++;
+            }
+            
+            peopleBitmask /= 2;
+            currBit++;
+        }
+        
+        return numPeople;
+    }
+};
+
+class TLESolution {
+public:
+    vector<int> smallestSufficientTeam(vector<string>& req_skills, vector<vector<string>>& people) {        
         memo = vector<vector<int>>();
         for (int i = 0; i < people.size(); ++i) {
             memo.push_back(vector<int>(getBitmaskSize(req_skills), UNINITIALIZED));
