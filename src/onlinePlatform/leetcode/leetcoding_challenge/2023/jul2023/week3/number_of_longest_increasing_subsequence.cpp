0e6e4131f8bcb14b82a23d12a9f90aad82dc9a4f
htbkoo <h1017760+github@connect.hku.hk>
2023-07-21 02:48:18 +0200

    solved `leetcode/leetcoding_challenge/2023/jul2023/week3/number_of_longest_increasing_subsequence`

--- src/onlinePlatform/leetcode/leetcoding_challenge/2023/jul2023/week3/number_of_longest_increasing_subsequence.cpp
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/jul2023/week3/number_of_longest_increasing_subsequence.cpp
@@ -17,6 +17,119 @@ public:
         unordered_map<int, pair<int, int>> lengths;
         
         for (auto num: nums) {
+            // cout << "WIP " << num <<  ": " << lengths.size() << endl;
+            unordered_map<int, pair<int, int>> newLengths;
+            
+            for (auto& [key, p]: lengths) {
+                auto& [length, count] = p;
+                
+                if (num > key) {
+                    if (lengths.count(num) != 0) {
+                        auto& [numLength, numCount] = lengths[num];
+                        if (length + 1 > numLength) {
+                            lengths[num] = pair{length + 1, count};
+                        } else if (length + 1 == numLength) {
+                            numCount += count;
+                        } 
+                    } else {
+                        if (newLengths.count(num) == 0) {
+                            newLengths[num] = pair{length + 1, count};
+                        } else {
+                             auto& [numLength, numCount] = newLengths[num];
+                            if (length + 1 > numLength) {
+                                newLengths[num] = pair{length + 1, count};
+                            } else if (length + 1 == numLength) {
+                                numCount += count;
+                            }   
+                        }
+                    }
+                }
+            }
+            
+            for (auto& [key, p]: newLengths) {
+                lengths[key] = newLengths[key];
+            }
+            
+            if (lengths.count(num) == 0) {
+                lengths[num] = pair{1, 1};
+            } else if (lengths[num].first == 1) {
+                lengths[num].second++;
+            }
+        }
+        
+        int longestLength = 0;
+        int answer = 0;
+        
+        for (auto& [key, p]: lengths) {
+            auto& [length, count] = p;
+
+            // cout << "end: " << key << " " << length << " " << count << endl;
+
+            if (length > longestLength) {
+                longestLength = length;
+                answer = count;
+            } else if (length == longestLength) {
+                answer += count;
+            }
+        }
+        
+        return answer;
+    }
+};
+
+class WASolution2 {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        unordered_map<int, pair<int, int>> lengths;
+        
+        for (auto num: nums) {
+            for (auto [key, p]: lengths) {
+                auto [length, count] = p;
+                
+                if (num > key) {
+                    if (lengths.count(num) == 0) {
+                        lengths[num] = pair{length + 1, count};
+                    } else {
+                        auto [numLength, numCount] = lengths[num];
+                        if (length + 1 > numLength) {
+                            lengths[num] = pair{length + 1, count};
+                        } else if (length + 1 == numLength) {
+                            lengths[num].second += count;
+                        } 
+                    }
+                }
+            }
+            if (lengths.count(num) == 0) {
+                lengths[num] = pair{1, 1};
+            } else if (lengths[num].first == 1) {
+                lengths[num].second++;
+            }
+        }
+        
+        int longestLength = 0;
+        int answer = 0;
+        
+        for (auto& [key, p]: lengths) {
+            auto& [length, count] = p;
+
+            if (length > longestLength) {
+                longestLength = length;
+                answer = count;
+            } else if (length == longestLength) {
+                answer += count;
+            }
+        }
+        
+        return answer;
+    }
+};
+
+class WASolution {
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        unordered_map<int, pair<int, int>> lengths;
+        
+        for (auto num: nums) {
             for (auto& [key, p]: lengths) {
                 auto& [length, count] = p;
 
