a7eebd842294617853cd6a6a132feb454d704a9b
htbkoo <h1017760+github@connect.hku.hk>
2023-08-10 02:41:33 +0200

    solved `leetcode/leetcoding_challenge/2023/aug2023/week2/search_in_rotated_sorted_array_ii`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/aug2023/week2/search_in_rotated_sorted_array_ii.cpp
@@ -0,0 +1,19 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        return unordered_set<int>(nums.begin(), nums.end()).count(target) > 0;
+    }
+};
