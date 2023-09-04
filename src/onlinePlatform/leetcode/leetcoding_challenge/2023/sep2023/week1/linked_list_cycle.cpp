61dc506ee33f056bd9b44f93e105b0b8fd33e1f3
htbkoo <h1017760+github@connect.hku.hk>
2023-09-04 09:28:37 +0200

    solved `leetcode/leetcoding_challenge/2023/sep2023/week1/linked_list_cycle`
    (cherry picked from commit 51d928fd6f04c70aeafe1e47401c1b98c68fce92)

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/sep2023/week1/linked_list_cycle.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == nullptr) {
+            return false;
+        }
+        
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+};
