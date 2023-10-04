5261d5fbdb74c85e84fcea6dd21fdae72b1700e4
htbkoo <h1017760+github@connect.hku.hk>
2023-10-04 10:48:48 +0200

    solved `leetcode/leetcoding_challenge/2023/oct2023/week1/design_hash_map.cpp`
    (cherry picked from commit 3cd6e60d87cb9e03147dd1ac04c776c4d1e101a4)

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2023/oct2023/week1/design_hash_map.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+class MyHashMap {
+public:
+    vector<int> arr;
+    MyHashMap() {
+        arr = vector<int>(1e6 + 1, -1);
+    }
+    
+    void put(int key, int value) {
+        arr[key] = value;
+    }
+    
+    int get(int key) {
+        return arr[key];
+    }
+    
+    void remove(int key) {
+        arr[key] = -1;
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
