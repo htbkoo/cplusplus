e44ba4b24256b4cd97ff53d2a55d9e9842f167ce
htbkoo <h1017760+github@connect.hku.hk>
2024-02-08 09:28:43 +0100

    solved `leetcode/leetcoding_challenge/2024/feb2024/weekly/paint_fence.cpp`

--- src/onlinePlatform/leetcode/leetcoding_challenge/2024/feb2024/weekly/paint_fence.cpp
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2024/feb2024/weekly/paint_fence.cpp
@@ -21,6 +21,68 @@ public:
             return 0;
         }
 
+        if (n == 1) {
+            return k;
+        }
+
+        if (n == 2) {
+            return k * k;
+        }
+
+        if (n == 3) {
+            return k * k * k - k;
+        }
+
+        memo = vector<vector<vector<int>>>();
+        for (int i = 0; i < n; ++i) {
+            memo.push_back(vector<vector<int>>());
+            for (int j = 0; j <= k; ++j) {
+                memo.back().push_back(vector<int>(k + 1, UNINITIALIZED));
+            }
+        }
+
+        int NO_COLOR = k;
+        return count(n, k, 0, NO_COLOR, NO_COLOR);
+    }
+
+private:
+    vector<vector<vector<int>>> memo;
+
+    int count(int n, int k, int i, int prevprev, int prev) {
+        if (i >= n) {
+            return 1;
+        }
+
+        if (memo[i][prevprev][prev] == UNINITIALIZED) {
+            memo[i][prevprev][prev] = 0;
+            for (int j = 0; j < k; ++j) {
+                bool noThreeConsecutive = j != prevprev || j != prev;
+                if (noThreeConsecutive) {
+                    memo[i][prevprev][prev] += count(n, k, i + 1, prev, j);
+                }
+            }
+        }
+
+        return memo[i][prevprev][prev];
+    }
+
+    int sum(vector<int>& v) {
+        int total = 0;
+        for (auto item: v) {
+            total += item;
+        }
+        return total;
+    }
+};
+
+class WIPSolution {
+public:
+    int numWays(int n, int k) {
+        if (n == 0 || k == 0) {
+            // TODO: confirm this, and what if n < 0 or k < 0?
+            return 0;
+        }
+
         vector<vector<int>> prev;
         for (int j = 0; j <= k; ++j) {
             prev.push_back(vector<int>(k + 1, 0));
