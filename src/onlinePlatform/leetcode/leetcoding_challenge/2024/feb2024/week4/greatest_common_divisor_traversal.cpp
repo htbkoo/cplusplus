cb577d818b65bb23ba1d4f255fb95ab2931f3b5b
htbkoo <h1017760+github@connect.hku.hk>
2024-02-25 11:08:22 +0100

    WIP `leetcode/leetcoding_challenge/2024/feb2024/week4/greatest_common_divisor_traversal.cpp`

--- src/onlinePlatform/leetcode/leetcoding_challenge/2024/feb2024/week4/greatest_common_divisor_traversal.cpp
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2024/feb2024/week4/greatest_common_divisor_traversal.cpp
@@ -10,25 +10,30 @@
 #include <sstream>
 #include <iostream> // includes cin to read from stdin and cout to write to stdout
 using namespace std; // since cin and cout are both in namespace std, this saves some text
+
 const int LARGEST_NUM = 1e5;
 
 vector<int> primes;
-vector<int> isPrimes(LARGEST_NUM + 1, false);
 
-for (int i = 2; i <= LARGEST_NUM; ++i) {
-    isPrimes[i] = true;
-    for (auto p: primes) {
-        if (i % p == 0) {
-            isPrimes[i] = false;
-            break;
-        }
-        if (p * p > i) {
-            break;
-        }
+void populatePrimes() {
+    if (primes.size() > 0) {
+        return;
     }
+    for (int i = 2; i <= LARGEST_NUM; ++i) {
+        bool isPrime = true;
+        for (auto p: primes) {
+            if (i % p == 0) {
+                isPrime = false;
+                break;
+            }
+            if (p * p > i) {
+                break;
+            }
+        }
 
-    if (isPrimes[i]) {
-        primes.push_back(i);
+        if (isPrime) {
+            primes.push_back(i);
+        }
     }
 }
 
@@ -36,10 +41,14 @@ for (int i = 2; i <= LARGEST_NUM; ++i) {
 class Solution {
 public:
     bool canTraverseAllPairs(vector<int>& nums) {
+        populatePrimes();
+
         for (int i = 0; i < nums.size(); ++i) {
             parents.push_back(i);
         }
         sizes = vector<int>(nums.size(), 1);
+
+        int largestSetSize = 1;
 
         int largest = *max_element(begin(nums), end(nums));
         for (auto p: primes) {
@@ -54,16 +63,40 @@ public:
                     if (rep == -1) {
                         rep = i;
                     }
-                    unionSets();
+                    largestSetSize = max(largestSetSize, unionSets(i, rep));
                 }
             }
         }
 
+        return largestSetSize == nums.size();
     }
 
 private:
     vector<int> parents;
     vector<int> sizes;
 
+    int find(int x) {
+        if (parents[x] != x) {
+            parents[x] = find(parents[x]);
+        }
+        return parents[x];
+    }
 
+    int unionSets(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x != y) {
+            if (sizes[y] > sizes[x]) {
+                int temp = y;
+                y = x;
+                x = temp;
+            }
+
+            parents[y] = x;
+            sizes[x] += sizes[y];
+        }
+
+        return sizes[x];
+    }
 };
