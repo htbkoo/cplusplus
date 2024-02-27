9adc39901a31ae5c742de04c216dced3a659bffd
htbkoo <h1017760+github@connect.hku.hk>
2024-02-27 08:45:26 +0100

    solved `leetcode/leetcoding_challenge/2024/feb2024/week4/diameter_of_binary_tree.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2024/feb2024/week4/diameter_of_binary_tree.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int diameterOfBinaryTree(TreeNode* root) {
+        if (root == nullptr) {
+            return 0;
+        }
+
+        getHeight(root);
+
+        return answer;
+    }
+
+private:
+    int answer;
+
+    int getHeight(TreeNode* root) {
+        if (root == nullptr) {
+            return -1;
+        }
+
+        int left = 1 + getHeight(root->left);
+        int right = 1 + getHeight(root->right);
+
+        answer = max(answer, left + right);
+
+        return max(left, right);
+    }
+};
