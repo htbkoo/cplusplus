ddce7b4a8a29873f627c2c814aafd1ce9fd5bc63
htbkoo <h1017760+github@connect.hku.hk>
2024-03-22 05:42:08 +0100

    solved `leetcode/leetcoding_challenge/2024/mar2024/week4/palindrome_linked_list.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2024/mar2024/week4/palindrome_linked_list.cpp
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+struct ListNode {
+   int val;
+   ListNode *next;
+   ListNode() : val(0), next(nullptr) {}
+   ListNode(int x) : val(x), next(nullptr) {}
+   ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        vector<int> v;
+        while (head != nullptr) {
+            v.push_back(head->val);
+            head = head->next;
+        }
+
+        int lo = 0;
+        int hi = v.size() - 1;
+
+        while (lo < hi) {
+            if (v[lo] != v[hi]) {
+                return false;
+            }
+            lo++;
+            hi--;
+        }
+
+        return true;
+    }
+};
