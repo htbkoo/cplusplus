7bdee3781e17fd7b84e395b46f7601c57f161567
htbkoo <h1017760+github@connect.hku.hk>
2024-04-27 15:27:47 +0200

    feat: Add `leetcode/leetcoding_challenge/2024/apr2024/week4/freedom_trail.cpp`

--- src/onlinePlatform/leetcode/leetcoding_challenge/2024/apr2024/week4/freedom_trail.cpp
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2024/apr2024/week4/freedom_trail.cpp
@@ -11,10 +11,54 @@
 #include <sstream>
 #include <iostream> // includes cin to read from stdin and cout to write to stdout
 using namespace std; // since cin and cout are both in namespace std, this saves some text
+
 const int UNINITIALIZED = -1;
 const int INVALID = numeric_limits<int>::max() / 2;
 
 class Solution {
+public:
+    int findRotateSteps(string ring, string key) {
+        deque<pair<int, int>> q;
+        int START_RING_POS = 0;
+        q.push_back(pair{0, START_RING_POS});
+        
+        set<pair<int, int>> visited;
+        
+        int answer = 0;
+        while (q.size() > 0) {
+            int count = q.size();
+            
+            for (int i = 0; i < count; ++i) {
+                auto state = q.front();
+                q.pop_front();
+
+                auto [k, r] = state;
+                if (k >= key.size()) {
+                    return answer;
+                }
+                
+                if (visited.count(state) > 0) {
+                    continue;
+                }
+                visited.insert(state);
+                
+                if (key[k] == ring[r]) {
+                    q.push_back(pair{k + 1, r});
+                } else {
+                    q.push_back(pair{k, (r + ring.size() + 1) % ring.size()});
+                    q.push_back(pair{k, (r + ring.size() - 1) % ring.size()});
+                }
+            }
+            
+            answer++;
+        }
+        
+        // TODO: confirm this
+        return -1;
+    }
+};
+
+class WASolution {
 public:
     int findRotateSteps(string ring, string key) {
         memo = vector<vector<int>>();
@@ -33,15 +77,15 @@ private:
     vector<vector<bool>> visited;
     
     int findMin(string& ring, string& key, int k, int r) {
+        if (k >= key.size()) {
+            return 0;
+        }
+        
         if (visited[k][r]) {
             return INVALID;
         }
         visited[k][r] = true;
             
-        if (k >= key.size()) {
-            return 0;
-        }
-        
         if (memo[k][r] == UNINITIALIZED) {
             if (key[k] == ring[r]) {
                 return 1 + findMin(ring, key, k + 1, r);
@@ -55,7 +99,6 @@ private:
         
         return memo[k][r];
     }
-    
 };
 
 int main() {
