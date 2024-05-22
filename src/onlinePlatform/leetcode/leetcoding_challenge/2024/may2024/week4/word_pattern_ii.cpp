6051971d61279716529d63bdb7057fbfdacef236
htbkoo <h1017760+github@connect.hku.hk>
2024-05-22 06:53:13 +0200

    feat(LeetCode): Solve `leetcode/leetcoding_challenge/2024/may2024/week4/word_pattern_ii.cpp`

--- src/onlinePlatform/leetcode/leetcoding_challenge/2024/may2024/week4/word_pattern_ii.cpp
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2024/may2024/week4/word_pattern_ii.cpp
@@ -33,6 +33,62 @@ private:
             return false;
         }
 
+        char ch = pattern[i];
+
+        if (mapping.count(ch) > 0) {
+            string mapped = mapping[ch];
+            for (int m = 0; m < mapped.size(); ++m) {
+                if (j + m >= s.size() || s[j + m] != mapped[m]) {
+                    return false;
+                }
+            }
+            return canMatch(pattern, s, i + 1, j + mapped.size());
+        }
+
+        for (int nextJ = j + 1; nextJ <= s.size(); ++nextJ) {
+            int count = nextJ - j;
+
+            string ss = s.substr(j, count);
+
+            if (used.count(ss) == 0) {
+                mapping[ch] = ss;
+                used.insert(ss);
+
+                if (canMatch(pattern, s, i + 1, nextJ)) {
+                    return true;
+                }
+
+                used.erase(ss);
+                mapping.erase(ch);
+            }
+        }
+
+        return false;
+    }
+};
+
+class WASolution {
+public:
+    bool wordPatternMatch(string pattern, string s) {
+        mapping = unordered_map<char, string>();
+        used = unordered_set<string>();
+
+        return canMatch(pattern, s, 0, 0);
+    }
+
+private:
+    unordered_map<char, string> mapping;
+    unordered_set<string> used;
+
+    bool canMatch(string& pattern, string& s, int i, int j) {
+        if (i == pattern.size() && j == s.size()) {
+            return true;
+        }
+
+        if (i == pattern.size() || j == s.size()) {
+            return false;
+        }
+
         for (int nextJ = j + 1; nextJ <= s.size(); ++nextJ) {
             char ch = pattern[i];
             if (mapping.count(ch) > 0) {
