8f0ed0a1c58917f4ed373af4a7e32625a67dcfbe
htbkoo <h1017760+github@connect.hku.hk>
2024-06-25 09:16:21 +0200

    feat(LeetCode): Solve `leetcode/leetcoding_challenge/2024/jun2024/week4/binary_search_tree_to_greater_sum_tree.cpp`

---
+++ src/onlinePlatform/leetcode/leetcoding_challenge/2024/jun2024/week4/binary_search_tree_to_greater_sum_tree.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <limits>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <iterator>
+#include <sstream>
+#include <iostream> // includes cin to read from stdin and cout to write to stdout
+using namespace std; // since cin and cout are both in namespace std, this saves some text
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* bstToGst(TreeNode* root) {
+        // TODO: assume we can mutate the given tree, otherwise we should clone the tree beforehand
+        addSum(root, 0);
+
+        return root;
+    }
+
+private:
+    int addSum(TreeNode* root, int prevSum) {
+        if (root == nullptr) {
+            return prevSum;
+        }
+
+        auto result = addSum(root->right, prevSum);
+        result += root->val;
+        root->val = result;
+        if (root->left) {
+            result = addSum(root->left, result);
+        }
+        return result;
+    }
+};
