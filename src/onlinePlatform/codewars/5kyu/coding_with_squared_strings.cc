875e73b68a213f4d09a5feaca49921d2280b8000
htbkoo <11581279+htbkoo@users.noreply.github.com>
2025-05-10 19:28:17 +0200

    feat(Codewars): WIP `onlinePlatform/codewars/5kyu/coding_with_squared_strings.cc`

--- src/onlinePlatform/codewars/5kyu/coding_with_squared_strings.cc
+++ src/onlinePlatform/codewars/5kyu/coding_with_squared_strings.cc
@@ -27,11 +27,71 @@ std::string joinVectorCharVector(const std::vector<std::vector<char> > &strings,
         v.push_back(char_string);
     }
 
-
-
     return joinStringVector(v, delimiter);
 }
 
+void rotateClockwise(std::vector<std::vector<char> > &v, int layer) {
+    // TODO: assume v is square-shaped
+    int n = v.size();
+    int top = layer;
+    int bottom = n - 1 - layer;
+    int left = layer;
+    int right = n - 1 - layer;
+
+    if (left >= right) {
+      return;
+    }
+
+    int d = 0;
+    while (left + d < right) {
+      auto t = v[top][left + d];
+      v[top][left + d] = v[bottom - d][left];
+      v[bottom - d][left] = v[bottom][right - d];
+      v[bottom][right - d] = v[top + d][right];
+      v[top + d][right] = t;
+
+      d++;
+    }
+    rotateClockwise(v, layer + 1);
+}
+
+void rotateCounterClockwise(std::vector<std::vector<char> > &v, int layer) {
+    // TODO: assume v is square-shaped
+    int n = v.size();
+    int top = layer;
+    int bottom = n - 1 - layer;
+    int left = layer;
+    int right = n - 1 - layer;
+
+    if (left >= right) {
+      return;
+    }
+
+    int d = 0;
+    while (left + d < right) {
+      auto t = v[top][left + d];
+      v[top][left + d] =v[top + d][right];
+      v[top + d][right] = v[bottom][right - d];
+      v[bottom][right - d] = v[bottom - d][left];
+      v[bottom - d][left] = t;
+
+      d++;
+    }
+    rotateClockwise(v, layer + 1);
+}
+
+std::vector<std::vector<char>> toCharsVectors(const std::string &s) {
+  int n = (int) std::sqrt(s.length());
+
+  std::vector<std::vector<char>> v;
+  for (int i = 0; i < s.length(); i += n) {
+    auto ss = s.substr(i, n);
+    std::vector<char> chars(ss.begin(), ss.end());
+    v.push_back(chars);
+  }
+
+  return v;
+}
 
 char FILLER_CHAR = (char) 11;
 std::string FILLER(1, FILLER_CHAR);
@@ -43,17 +103,32 @@ public:
 
         std::vector temp(n, std::vector<char>(n, FILLER_CHAR));
 
+        // fill strng into temp
         for (int i = 0; i < strng.length(); ++i) {
             int y = i / n;
             int x = i % n;
             temp[y][x] = strng[i];
         }
 
+        // rotate 90 degrees clockwise
+        rotateClockwise(temp, 0);
 
         return joinVectorCharVector(temp, "\n");
     }
 
     static std::string decode(const std::string &strng) {
-        return "";
+      auto temp = toCharsVectors(strng);
+
+      // rotate 90 degrees counter-clockwise
+      rotateCounterClockwise(temp, 0);
+
+      auto s = joinVectorCharVector(temp, "");
+
+      std::string::size_type loc = s.find( FILLER, 0 );
+      if( loc != std::string::npos ) {
+        return s;
+      } else {
+        return s.substr(0, loc);
+      }
     }
 };
